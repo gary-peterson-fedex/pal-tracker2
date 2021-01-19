2beef37aceececcabe903d774e66c2c024e4f515
Ubuntu <ubuntu@ip-172-31-95-103.ec2.internal>
2021-01-19 18:38:10 +0000

    mvc stub

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,31 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.ResponseEntity;
+
+public class TimeEntryController{
+    private TimeEntryRepository repository;
+
+    public TimeEntryController(TimeEntryRepository repository) {
+        this.repository = repository;
+    }
+
+    public ResponseEntity create(TimeEntry timeEntry){
+        return null;
+    }
+
+    public ResponseEntity update(long id, TimeEntry timeEntry){
+        return null;
+    }
+
+    public ResponseEntity list(){
+        return null;
+    }
+
+    public ResponseEntity read(long id){
+        return null;
+    }
+
+    public ResponseEntity delete(long id){
+        return null;
+    }
+}
