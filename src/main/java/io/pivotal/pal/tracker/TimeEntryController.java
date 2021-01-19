e2b2161f555281bdec11a1b9f4b605f75f60da58
Ubuntu <ubuntu@ip-172-31-95-103.ec2.internal>
2021-01-19 20:33:15 +0000

    some tests pass

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,6 +1,11 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.*;
 
 public class TimeEntryController{
     private TimeEntryRepository repository;
@@ -9,24 +14,34 @@ public class TimeEntryController{
         this.repository = repository;
     }
 
-    public ResponseEntity create(TimeEntry timeEntry){
-        return null;
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
+        return created(null)
+                .body(repository.create(timeEntryToCreate));
     }
 
-    public ResponseEntity update(long id, TimeEntry timeEntry){
-        
-        return null;
+    public ResponseEntity<TimeEntry> update(@PathVariable long id, @RequestBody TimeEntry timeEntry) {
+        var timeEntryUpdated = repository.update(id, timeEntry);
+
+        return timeEntryUpdated == null ?
+                notFound().build() :
+                ok(timeEntryUpdated);
     }
 
-    public ResponseEntity list(){
-        return null;
+    public ResponseEntity<List<TimeEntry>> list() {
+        return ok(repository.list());
     }
 
-    public ResponseEntity read(long id){
-        return null;
+    public ResponseEntity<TimeEntry> read(@PathVariable long id) {
+        var timeEntryFound = repository.find(id);
+
+        return timeEntryFound == null ?
+                notFound().build() :
+                ok(timeEntryFound);
     }
 
-    public ResponseEntity delete(long id){
-        return null;
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        repository.delete(id);
+
+        return noContent().build();
     }
 }
